eda1a7947972a43f19b1c8b8c954db4dd1cab1a3
luoguangcong <luoguangcong@corp.netease.com>
2023-09-03 16:12:26 +0800

    feat: define service of contribution/eas

---
+++ src/services/contribution.ts
@@ -0,0 +1,35 @@
+import { request } from '@/common/request';
+import { IContribution } from '@/services/types';
+import { PageListParams } from '@/common/types';
+
+export interface ICreateContributionParams {
+	signature: string;
+	operatorId: string;
+	detail: string;
+	proof: string;
+	credit: number;
+	toIds: string[];
+	projectId: string;
+}
+
+export interface IUpdateContributionParams {
+	signature: string;
+	operatorId: string;
+	type: 'claim';
+}
+
+export const createContribution = (params: ICreateContributionParams) => {
+	return request.post<IContribution>('contribution/create', 1, params);
+};
+
+export const editContribution = (cid: string, params: Omit<ICreateContributionParams, 'projectId'>) => {
+	return request.put<IContribution>(`contribution/${cid}/edit`, 1, params);
+};
+
+export const updateContributionStatus = (cid: string, params: IUpdateContributionParams) => {
+	return request.put<IContribution>(`contribution/${cid}/updateState`, 1, params);
+};
+
+export const getContributionList = (params: PageListParams & { projectId: string }) => {
+	return request<IContribution[]>('contribution/list', 1, params);
+};
---
+++ src/services/eas.ts
@@ -0,0 +1,5 @@
+import { request } from '@/common/request';
+
+export const getEasSignature = (params: { wallet: string, cid: string }) => {
+	return request<string>('eas/signature', 1, params);
+};
