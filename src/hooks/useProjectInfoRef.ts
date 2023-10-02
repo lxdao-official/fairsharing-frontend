809663dff05bea65a520e3a52186896beb3f743e
KieSun <zx597813039@gmail.com>
2023-10-02 14:30:49 +0800

    feat: add custom hooks

---
+++ src/hooks/useProjectInfoRef.ts
@@ -0,0 +1,14 @@
+import { useRef } from 'react';
+import { StepProfileRef } from '@/components/createProject/step/profile';
+import { StepStrategyRef } from '@/components/createProject/step/strategy';
+import { StepContributorRef } from '@/components/createProject/step/contributor';
+
+const useProjectInfoRef = () => {
+	const stepProfileRef = useRef<StepProfileRef | null>(null);
+	const stepStrategyRef = useRef<StepStrategyRef | null>(null);
+	const stepContributorRef = useRef<StepContributorRef | null>(null);
+
+	return { stepContributorRef, stepProfileRef, stepStrategyRef };
+};
+
+export default useProjectInfoRef;
