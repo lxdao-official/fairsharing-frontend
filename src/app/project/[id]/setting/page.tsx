e6218c63a07f902c93fbf52bcec5822d2b2ef791
KieSun <zx597813039@gmail.com>
2023-10-02 12:00:29 +0800

    feat: setting profile page

--- src/app/project/[id]/setting/page.tsx
+++ src/app/project/[id]/setting/page.tsx
@@ -1,17 +1,59 @@
 'use client';
 
 import useSWR from 'swr';
-import { Typography, Tabs, Tab } from '@mui/material';
+import { Typography, Tabs, Tab, Skeleton, Stack, Alert } from '@mui/material';
 
 import { StyledFlexBox } from '@/components/styledComponents';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import StepProfile, { StepProfileRef } from '@/components/createProject/step/profile';
+import { editProject, getContributorList, getMintRecord, getProjectDetail } from '@/services';
+import { useAccount } from 'wagmi';
+import { showToast } from '@/store/utils';
 
 export default function Setting({ params }: { params: { id: string } }) {
+	const stepProfileRef = useRef<StepProfileRef | null>(null);
+
+	const {
+		isLoading: detailLoading,
+		data,
+		mutate,
+	} = useSWR(['getProjectDetail', params.id], () => getProjectDetail(params.id));
+	const { isLoading: contributorsLoading, data: contributorsData } = useSWR(
+		['getContributorList', params.id],
+		() => getContributorList(params.id),
+		{
+			fallbackData: [],
+		},
+	);
+
+	const { address } = useAccount();
+
 	const [activeTab, setActiveTab] = useState('profile');
+
+	const isContributor = useMemo(
+		() => contributorsData?.some((item) => item.wallet === address),
+		[contributorsData, address],
+	);
 
 	const handleTabChange = useCallback((_: any, value: string) => {
 		setActiveTab(value);
 	}, []);
+
+	const handleProfileSubmit = useCallback(async () => {
+		const formData = stepProfileRef.current?.getFormData();
+		if (formData) {
+			const { name, intro, avatar } = formData;
+			await editProject({
+				id: params.id,
+				name,
+				intro,
+				logo: avatar,
+				votePeriod: data!.votePeriod,
+			});
+			showToast('Project profile updated successfully');
+			await mutate();
+		}
+	}, [data]);
 
 	return (
 		<div>
@@ -21,14 +63,9 @@ export default function Setting({ params }: { params: { id: string } }) {
 			<Tabs
 				value={activeTab}
 				onChange={handleTabChange}
-				TabIndicatorProps={{
-					style: {
-						backgroundColor: '#0F172A',
-					},
-				}}
 				sx={{
 					'.Mui-selected': {
-						color: `#0F172A`,
+						color: `#0F172A !important`,
 					},
 				}}
 			>
@@ -36,6 +73,40 @@ export default function Setting({ params }: { params: { id: string } }) {
 				<Tab value="strategy" label="Strategy" />
 				<Tab value="contributors" label="Contributors" />
 			</Tabs>
+			<div style={{ marginTop: 32 }}>
+				{detailLoading || contributorsLoading ? (
+					<Stack spacing={5}>
+						<Skeleton variant="circular" width={80} height={80} />
+						<Skeleton variant="rounded" width={300} height={60} />
+						<Skeleton variant="rounded" width={300} height={80} />
+					</Stack>
+				) : (
+					<>
+						{!isContributor ? (
+							<Alert
+								severity="info"
+								style={{ marginBottom: 32, display: 'flex', alignItems: 'center' }}
+								sx={{
+									backgroundColor: '#F5F5F5',
+									color: '#475569',
+									'.MuiAlert-icon': {
+										color: '#0F172A7A',
+									},
+								}}
+							>
+								You are in view only mode, to modify project settings connect with
+								an admin wallet.
+							</Alert>
+						) : null}
+						<StepProfile
+							ref={stepProfileRef}
+							data={data}
+							canEdit={isContributor}
+							onSave={handleProfileSubmit}
+						/>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
