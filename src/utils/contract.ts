892a2ef3234a6ccd53248f8862bf6a30824d4d2b
KieSun <zx597813039@gmail.com>
2023-11-12 20:14:35 +0800

    feat: opt code

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,36 +1,39 @@
 import { VoteApproveEnum, VoteSystemEnum } from '@/services';
 import { VoteStrategyABIMap, VoteStrategyMap } from '@/constant/contract';
 
-
 export function getVoteStrategyABI(voteApproveType: VoteApproveEnum) {
-	if (voteApproveType === VoteApproveEnum.DEFAULT) {
-		return VoteStrategyABIMap.RelativeV1;
-	} else if (voteApproveType === VoteApproveEnum.RELATIVE2) {
-		return VoteStrategyABIMap.RelativeV2;
-	} else if (voteApproveType === VoteApproveEnum.ABSOLUTE1) {
-		return VoteStrategyABIMap.AbsoluteV1;
-	} else if (voteApproveType === VoteApproveEnum.ABSOLUTE2) {
-		return VoteStrategyABIMap.AbsoluteV2;
-	} else {
-		return VoteStrategyABIMap.RelativeV1;
+	const { RelativeV1, RelativeV2, AbsoluteV2, AbsoluteV1 } = VoteStrategyABIMap;
+	switch (voteApproveType) {
+		case VoteApproveEnum.ABSOLUTE1:
+			return AbsoluteV1;
+		case VoteApproveEnum.ABSOLUTE2:
+			return AbsoluteV2;
+		case VoteApproveEnum.RELATIVE2:
+			return RelativeV2;
+		default:
+			return RelativeV1;
 	}
 }
 
 export function getVoteStrategyContract(voteApproveType: VoteApproveEnum): string {
-	if (voteApproveType === VoteApproveEnum.DEFAULT) {
-		return VoteStrategyMap.RelativeV1;
-	} else if (voteApproveType === VoteApproveEnum.RELATIVE2) {
-		return VoteStrategyMap.RelativeV2;
-	} else if (voteApproveType === VoteApproveEnum.ABSOLUTE1) {
-		return VoteStrategyMap.AbsoluteV1;
-	} else if (voteApproveType === VoteApproveEnum.ABSOLUTE2) {
-		return VoteStrategyMap.AbsoluteV2;
-	} else {
-		return VoteStrategyMap.RelativeV1;
+	const { RelativeV1, RelativeV2, AbsoluteV2, AbsoluteV1 } = VoteStrategyMap;
+	switch (voteApproveType) {
+		case VoteApproveEnum.ABSOLUTE1:
+			return AbsoluteV1;
+		case VoteApproveEnum.ABSOLUTE2:
+			return AbsoluteV2;
+		case VoteApproveEnum.RELATIVE2:
+			return RelativeV2;
+		default:
+			return RelativeV1;
 	}
 }
 
-export function getVoteThreshold(voteApproveType: VoteApproveEnum, forWeightOfTotal: number | string, differWeightOfTotal: number | string) {
+export function getVoteThreshold(
+	voteApproveType: VoteApproveEnum,
+	forWeightOfTotal: number | string,
+	differWeightOfTotal: number | string,
+) {
 	let voteThreshold = 50;
 	if (voteApproveType === VoteApproveEnum.ABSOLUTE1) {
 		voteThreshold = Number(forWeightOfTotal);
@@ -40,7 +43,11 @@ export function getVoteThreshold(voteApproveType: VoteApproveEnum, forWeightOfTo
 	return voteThreshold;
 }
 
-export function getVoteWeights(voteSystem: VoteSystemEnum, voteWeights: number[], memberNum: number) {
+export function getVoteWeights(
+	voteSystem: VoteSystemEnum,
+	voteWeights: number[],
+	memberNum: number,
+) {
 	if (voteSystem === VoteSystemEnum.EQUAL) {
 		const weight = Math.floor(100 / memberNum);
 		return Array(memberNum).fill(weight);
