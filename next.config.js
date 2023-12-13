c621ce3541952ebb2c4de8fc359db7685fe4c598
KieSun <zx597813039@gmail.com>
2023-12-13 18:07:14 +0800

    feat: list redirect to home

--- next.config.js
+++ next.config.js
@@ -7,46 +7,62 @@ const nextConfig = {
 		APP_ENV: process.env.APP_ENV,
 	},
 	compiler: {
-		removeConsole: process.env.APP_ENV === "Production" ? {
-			exclude: ['error'],
-		} : false,
+		removeConsole:
+			process.env.APP_ENV === 'Production'
+				? {
+						exclude: ['error'],
+				  }
+				: false,
 	},
 	modularizeImports: {
 		'@mui/icons-material': {
 			transform: '@mui/icons-material/{{member}}',
 		},
 	},
-	webpack: config => {
+	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/i,
 			issuer: /\.[jt]sx?$/,
-			use: [{
-				loader: '@svgr/webpack',
-				options: {
-					svgo: true,
-					svgoConfig: {
-						plugins: [
-							{
-								name: 'preset-default',
-								params: { overrides: { removeViewBox: false } },
-							},
-						],
+			use: [
+				{
+					loader: '@svgr/webpack',
+					options: {
+						svgo: true,
+						svgoConfig: {
+							plugins: [
+								{
+									name: 'preset-default',
+									params: { overrides: { removeViewBox: false } },
+								},
+							],
+						},
 					},
 				},
-			}],
-		})
+			],
+		});
 		config.resolve.fallback = { fs: false, net: false, tls: false };
 		config.externals.push('pino-pretty', 'lokijs', 'encoding');
 		return config;
 	},
 	async rewrites() {
-		return process.env.APP_ENV === 'Local' ? [
-			{
-				source: '/fs-api/:path*',
-				destination: `${process.env.NEXT_PUBLIC_API_HOST_PROXY}/:path*`,
-			},
-		] : []
+		return process.env.APP_ENV === 'Local'
+			? [
+					{
+						source: '/fs-api/:path*',
+						destination: `${process.env.NEXT_PUBLIC_API_HOST_PROXY}/:path*`,
+					},
+			  ]
+			: [];
 	},
-}
+	async redirects() {
+		return [
+			{
+				source: '/list',
+				destination: '/',
+				permanent: false,
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
