a7b37415fa668f8a3814547a11c6b5b124fcdf60
Bruce Xu <bruce.xu@lxdao.io>
2024-01-09 12:58:01 +1300

    Update gateway

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,29 +3,21 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React from 'react';
 import NextTopLoader from 'nextjs-toploader';
-
 import { AppBar, Toolbar, Typography } from '@mui/material';
-
 import { Analytics } from '@vercel/analytics/react';
-
 import Image from 'next/image';
-
 import Link from 'next/link';
+import { Img3Provider } from '@lxdao/img3';
 
 import ThemeRegistry from '@/components/themeRegistry/themeRegistry';
 import { RainbowProvider } from '@/components/rainbow/provider';
-
 import SimpleGlobalLoading from '@/components/loading';
-
 import SimpleSnackbar from '@/components/simpleSnackbar/snackbar';
-
 import User from '@/components/header/user';
-
 import NavLogo from '@/components/navLogo';
-
 import NavLayout from '@/components/navLayout';
-
 import { ZIndexMap } from '@/constant/style';
+import { defaultGateways } from '@/constant/img3';
 
 import styles from '../styles/layout.module.css';
 
@@ -43,38 +35,44 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<NextTopLoader />
 				<ThemeRegistry>
 					<RainbowProvider>
-						<AppBar
-							position="fixed"
-							sx={{
-								backgroundColor: 'white',
-								height: '64px',
-								borderBottom: '0.5px solid #CBD5E1',
-								boxShadow: 'none',
-								zIndex: ZIndexMap.AppBar,
-							}}
-							enableColorOnDark={true}
-						>
-							<Toolbar>
-								<Link
-									href={'/'}
-									style={{ display: 'flex', flex: 1, alignItems: 'center' }}
-								>
-									<NavLogo />
-									<Typography
-										variant="h4"
-										sx={{ flexGrow: 1, fontWeight: '500', marginLeft: '8px' }}
+						<Img3Provider defaultGateways={defaultGateways}>
+							<AppBar
+								position="fixed"
+								sx={{
+									backgroundColor: 'white',
+									height: '64px',
+									borderBottom: '0.5px solid #CBD5E1',
+									boxShadow: 'none',
+									zIndex: ZIndexMap.AppBar,
+								}}
+								enableColorOnDark={true}
+							>
+								<Toolbar>
+									<Link
+										href={'/'}
+										style={{ display: 'flex', flex: 1, alignItems: 'center' }}
 									>
-										FairSharing
-									</Typography>
-								</Link>
-								<User />
-							</Toolbar>
-						</AppBar>
-						<div className={styles.main}>
-							<NavLayout>{children}</NavLayout>
-						</div>
-						<SimpleGlobalLoading />
-						<SimpleSnackbar />
+										<NavLogo />
+										<Typography
+											variant="h4"
+											sx={{
+												flexGrow: 1,
+												fontWeight: '500',
+												marginLeft: '8px',
+											}}
+										>
+											FairSharing
+										</Typography>
+									</Link>
+									<User />
+								</Toolbar>
+							</AppBar>
+							<div className={styles.main}>
+								<NavLayout>{children}</NavLayout>
+							</div>
+							<SimpleGlobalLoading />
+							<SimpleSnackbar />
+						</Img3Provider>
 					</RainbowProvider>
 				</ThemeRegistry>
 				<Analytics />
