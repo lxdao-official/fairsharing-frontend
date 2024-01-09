26f209ede9ff88b6771474defd015e289dc2a6e8
Bruce Xu <bruce.xu@lxdao.io>
2024-01-09 13:02:11 +1300

    Remove provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,7 @@ import React from 'react';
 import NextTopLoader from 'nextjs-toploader';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import { Analytics } from '@vercel/analytics/react';
-import Image from 'next/image';
 import Link from 'next/link';
-import { Img3Provider } from '@lxdao/img3';
 
 import ThemeRegistry from '@/components/themeRegistry/themeRegistry';
 import { RainbowProvider } from '@/components/rainbow/provider';
@@ -17,7 +15,6 @@ import User from '@/components/header/user';
 import NavLogo from '@/components/navLogo';
 import NavLayout from '@/components/navLayout';
 import { ZIndexMap } from '@/constant/style';
-import { defaultGateways } from '@/constant/img3';
 
 import styles from '../styles/layout.module.css';
 
@@ -35,44 +32,42 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<NextTopLoader />
 				<ThemeRegistry>
 					<RainbowProvider>
-						<Img3Provider defaultGateways={defaultGateways}>
-							<AppBar
-								position="fixed"
-								sx={{
-									backgroundColor: 'white',
-									height: '64px',
-									borderBottom: '0.5px solid #CBD5E1',
-									boxShadow: 'none',
-									zIndex: ZIndexMap.AppBar,
-								}}
-								enableColorOnDark={true}
-							>
-								<Toolbar>
-									<Link
-										href={'/'}
-										style={{ display: 'flex', flex: 1, alignItems: 'center' }}
+						<AppBar
+							position="fixed"
+							sx={{
+								backgroundColor: 'white',
+								height: '64px',
+								borderBottom: '0.5px solid #CBD5E1',
+								boxShadow: 'none',
+								zIndex: ZIndexMap.AppBar,
+							}}
+							enableColorOnDark={true}
+						>
+							<Toolbar>
+								<Link
+									href={'/'}
+									style={{ display: 'flex', flex: 1, alignItems: 'center' }}
+								>
+									<NavLogo />
+									<Typography
+										variant="h4"
+										sx={{
+											flexGrow: 1,
+											fontWeight: '500',
+											marginLeft: '8px',
+										}}
 									>
-										<NavLogo />
-										<Typography
-											variant="h4"
-											sx={{
-												flexGrow: 1,
-												fontWeight: '500',
-												marginLeft: '8px',
-											}}
-										>
-											FairSharing
-										</Typography>
-									</Link>
-									<User />
-								</Toolbar>
-							</AppBar>
-							<div className={styles.main}>
-								<NavLayout>{children}</NavLayout>
-							</div>
-							<SimpleGlobalLoading />
-							<SimpleSnackbar />
-						</Img3Provider>
+										FairSharing
+									</Typography>
+								</Link>
+								<User />
+							</Toolbar>
+						</AppBar>
+						<div className={styles.main}>
+							<NavLayout>{children}</NavLayout>
+						</div>
+						<SimpleGlobalLoading />
+						<SimpleSnackbar />
 					</RainbowProvider>
 				</ThemeRegistry>
 				<Analytics />
