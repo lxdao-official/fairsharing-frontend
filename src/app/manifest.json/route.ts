3a83c2263f530008a43e8a2d6a582afceb211d50
LGC <lguangcong@163.com>
2024-01-14 19:19:24 +0800

    feat(safe): add manifest.json api

---
+++ src/app/manifest.json/route.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
+export async function GET(request: Request, res: Response) {
+	const filePath = path.join(process.cwd(), 'public/safe', 'manifest.json');
+	const jsonData = fs.readFileSync(filePath, 'utf8');
+	return new Response(jsonData, {
+		status: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET",
+			"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+		},
+	});
+}
